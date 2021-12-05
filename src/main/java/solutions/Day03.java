1bd54927d5e53b98883519619a3ca41b0fee94c0
Eva Parish <eparish@squarespace.com>
2021-12-05 16:48:00 -0500

    Day 3 part 2

--- src/main/java/solutions/Day03.java
+++ src/main/java/solutions/Day03.java
@@ -1,5 +1,6 @@
 package main.java.solutions;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +24,6 @@ public class Day03 implements Day {
                 }
             }
         }
-
         String gammaRate = "";
         String epsilonRate = "";
         for (Bit bit : listOfBits) {
@@ -34,16 +34,86 @@ public class Day03 implements Day {
                 gammaRate = gammaRate.concat("0");
                 epsilonRate = epsilonRate.concat("1");
             }
-            System.out.println("Gamma rate " + gammaRate + " and epsilon rate " + epsilonRate);
+            // System.out.println("Gamma rate " + gammaRate + " and epsilon rate " + epsilonRate);
         }
         int gammaRateInDecimal = Integer.parseInt(gammaRate, 2);
         int epsilonRateInDecimal = Integer.parseInt(epsilonRate, 2);
         return Integer.toString(gammaRateInDecimal*epsilonRateInDecimal);
     }
 
+    /* Part 2 answer "inspired by" @domoore's */
     @Override
     public String part2(List<String> input) {
-        return null;
+        String oxygenGeneratorRating = "";
+        String co2ScrubberRating = "";
+
+        List<String> oxOutput = new ArrayList<>(input);
+        List<String> coOutput = new ArrayList<>(input);
+
+        for (int bitPosition = 0; bitPosition < oxOutput.get(0).length(); bitPosition++) {
+            //System.out.println("Times through the loop: " + bitPosition);
+            // System.out.println("OxOutput is currently: " + oxOutput);
+            if (oxOutput.size() == 1) {
+                oxygenGeneratorRating = oxOutput.get(0);
+                break;
+            }
+            int zeroes = 0;
+            int ones = 0;
+            for (String log : oxOutput) {
+                if (log.charAt(bitPosition) == '0') {
+                    zeroes += 1;
+                } else {
+                    ones += 1;
+                }
+            }
+            int num;
+            if (zeroes > ones) {
+                num = 0;
+            } else {
+                num = 1;
+            }
+            oxOutput = trimFailingNumbers(oxOutput, bitPosition, num);
+        }
+
+        for (int bitPosition = 0; bitPosition < coOutput.get(0).length(); bitPosition++) {
+            if (coOutput.size() == 1) {
+                co2ScrubberRating = coOutput.get(0);
+                break;
+            }
+            int zeroes = 0;
+            int ones = 0;
+            for (String log : coOutput) {
+                if (log.charAt(bitPosition) == '0') {
+                    ones += 1;
+                } else {
+                    zeroes += 1;
+                }
+            }
+            int num;
+            if (zeroes >= ones) {
+                num = 0;
+            } else {
+                num = 1;
+            }
+            coOutput = trimFailingNumbers(coOutput, bitPosition, num);
+        }
+
+        // System.out.println("Ox:  " + oxygenGeneratorRating);
+        // System.out.println("CO2: " + co2ScrubberRating);
+        Long oxInDecimal = Long.parseLong(oxygenGeneratorRating, 2);
+        Long coInDecimal = Long.parseLong(co2ScrubberRating, 2);
+        Long answer = oxInDecimal * coInDecimal;
+        return Long.toString(answer);
+    }
+
+    public List<String> trimFailingNumbers(List<String> list, int position, int number) {
+        List<String> newList = new ArrayList<>();
+        for (String item : list) {
+            if (Character.getNumericValue(item.charAt(position)) == number) {
+                newList.add(item);
+            }
+        }
+        return newList;
     }
 
     private static class Bit {
